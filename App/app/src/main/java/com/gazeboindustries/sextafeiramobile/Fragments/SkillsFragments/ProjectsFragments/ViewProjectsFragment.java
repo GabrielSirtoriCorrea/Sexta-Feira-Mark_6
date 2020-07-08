260964def39ee9b06c49dd8a2df3c8d42d56ad78
Gazebo <gabrielsirtori.correa@gmail.com>
2020-07-07 22:24:00 -0300

    funcoesImplementadas

--- App/app/src/main/java/com/gazeboindustries/sextafeiramobile/Fragments/SkillsFragments/ProjectsFragments/ViewProjectsFragment.java
+++ App/app/src/main/java/com/gazeboindustries/sextafeiramobile/Fragments/SkillsFragments/ProjectsFragments/ViewProjectsFragment.java
@@ -1,10 +1,12 @@
 package com.gazeboindustries.sextafeiramobile.Fragments.SkillsFragments.ProjectsFragments;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -14,9 +16,12 @@ import androidx.fragment.app.Fragment;
 import com.gazeboindustries.sextafeiramobile.R;
 
 public class ViewProjectsFragment extends Fragment {
-    Intent intent;
-    EditText txtProject;
-    EditText txtRepository;
+    private Intent intent;
+    private EditText txtProject;
+    private EditText txtRepository;
+
+    private Button btnEditSend;
+    private Button btnDeleteCancel;
 
     @Nullable
     @Override
@@ -26,10 +31,55 @@ public class ViewProjectsFragment extends Fragment {
         txtProject = view.findViewById(R.id.txtViewProject);
         txtRepository = view.findViewById(R.id.txtViewRepository);
 
+        btnEditSend = view.findViewById(R.id.btnEditProject);
+        btnDeleteCancel = view.findViewById(R.id.btnRemoveProject);
+
         intent = getActivity().getIntent();
 
-        txtProject.setText(intent.getSerializableExtra("Project").toString());
-        txtRepository.setText(intent.getSerializableExtra("Repository").toString());
+        txtProject.setText(intent.getStringExtra("Project"));
+        txtRepository.setText(intent.getStringExtra("Repository"));
+
+        btnEditSend.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                if(btnEditSend.getText().equals("Editar")){
+                    txtProject.setEnabled(true);
+                    txtRepository.setEnabled(true);
+
+
+                    btnDeleteCancel.setText("Cancelar");
+                    btnEditSend.setText("Salvar");
+                }else{
+                    //connection = new ServerConnection();
+                    System.out.println("ENVIAR");
+                    //connection.sendRequest(connection.prepareRequest());
+                }
+            }
+        });
+
+        btnDeleteCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(btnDeleteCancel.getText().equals("Remover")){
+                    //show dialog
+                    System.out.println("Remove");
+
+                }else{
+                    txtProject.setEnabled(false);
+                    txtRepository.setEnabled(false);
+
+
+                    btnDeleteCancel.setText("Remover");
+                    btnEditSend.setText("Editar");
+
+                    txtProject.setText(intent.getStringExtra("Project"));
+                    txtRepository.setText(intent.getStringExtra("Repository"));
+
+                }
+            }
+        });
+
         return view;
     }
 }
