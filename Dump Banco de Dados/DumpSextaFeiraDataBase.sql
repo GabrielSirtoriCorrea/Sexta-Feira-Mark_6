2ad13118e8e77aff235ba98e4c3c3ee763c060e1
Gazebo <gabrielsirtori.correa@gmail.com>
2020-07-23 21:06:31 -0300

    GerandoInterfaceExecutavelJar

--- Dump Banco de Dados/DumpSextaFeiraDataBase.sql
+++ Dump Banco de Dados/DumpSextaFeiraDataBase.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `sextafeiradatabase` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `sextafeiradatabase`;
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sextafeiradatabase
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,9 +28,9 @@ CREATE TABLE `device` (
   `DeviceID` int NOT NULL AUTO_INCREMENT,
   `DeviceName` varchar(20) DEFAULT NULL,
   `DeviceDescription` text,
-  `DeviceJson` text,
+  `DeviceActions` int DEFAULT NULL,
   PRIMARY KEY (`DeviceID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `device` (
 
 LOCK TABLES `device` WRITE;
 /*!40000 ALTER TABLE `device` DISABLE KEYS */;
-INSERT INTO `device` VALUES (1,'Device','Description','Json'),(2,'newDevice','newDevice','newDevice'),(3,'newDevice','newDevice','newDevice');
+INSERT INTO `device` VALUES (1,'Friday','Kernel sexta-feira',10),(2,'Interface','Interface sexta-feira',10);
 /*!40000 ALTER TABLE `device` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +58,7 @@ CREATE TABLE `homeworkmanagement` (
   `HomeWorkDelivery` date DEFAULT NULL,
   `HomeWorkDescription` text,
   PRIMARY KEY (`HomeWorkID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `homeworkmanagement` (
 
 LOCK TABLES `homeworkmanagement` WRITE;
 /*!40000 ALTER TABLE `homeworkmanagement` DISABLE KEYS */;
-INSERT INTO `homeworkmanagement` VALUES (1,'Type','subject','homework','2020-06-02','desc'),(2,'newHomeWork','newHomeWork','newHomeWork','2020-06-02','newHomeWork');
+INSERT INTO `homeworkmanagement` VALUES (1,'Type','subject','homework','2030-06-02','desc'),(6,'t','t','t','2020-07-12','t');
 /*!40000 ALTER TABLE `homeworkmanagement` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +88,7 @@ CREATE TABLE `interactions` (
   `Response3` text,
   `Command` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`InteractionID`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +97,7 @@ CREATE TABLE `interactions` (
 
 LOCK TABLES `interactions` WRITE;
 /*!40000 ALTER TABLE `interactions` DISABLE KEYS */;
-INSERT INTO `interactions` VALUES (1,'ola','ola','ola','olá chefe','olá chefe','olá chefe',''),(2,'horas','sao','sao','','','','dateTime'),(3,'fech','tabela','tabela','','','','sendCloseToInterface'),(4,'mostr','interacoes','interacoes','Atualmente essas são as interações contidas no meu banco de dados','Essas são todas as interações contidas no meu banco de dados','Aqui estão as interações contidas no meu banco de dados','sendInteractionsToInterface'),(5,'mostr','tarefa','tarefa','Essas são todas as suas tarefas pendentes','Aqui estão as suas tarefas pendentes','Atualmente essas são suas tarefas pendentes','sendHomeWorksToInterface'),(6,'mostr','projeto','projeto','Esses são todos os projetos em andamento','Aqui estão os projetos em desenvolvimento','Atualmente esse são os projetos em desenvolvimento','sendProjectsToInterface'),(7,'mostr','device','device','Esses são os devices sincronizados','Aqui estão os devices sincronizados','Atualmente esse são os devices sincronizados com o meu sistema','sendDevicesToInterface'),(8,'fech','image','image','','','','sendCloseToInterface'),(9,'mostr','tabela','periodica','tabela periodica aberta','tabela periodica aberta','tabela periodica aberta','sendPeriodicTableToInterface'),(10,'mostr','mapa','brasil politico','Esse é o mapa político do Brasil','Esse é o mapa político do Brasil','Esse é o mapa político do Brasil','sendPoliticalBrazilToInterface'),(11,'mostr','distribuicao','eletronica','Aqui está a distribuição eletrônica dos elementos','Aqui está a distribuição eletrônica dos elementos','Aqui está a distribuição eletrônica dos elementos','sendEletronicDestToInterface'),(12,'mostr','tabela','angulos','Aqui está a tabela dos principais angulos na trigonometria','Aqui está a tabela dos principais angulos na trigonometria','Aqui está a tabela dos principais angulos na trigonometria','sendAnglesTableToInterface'),(13,'mostr','equacoes','fisica mecanica','Aqui estão as principais equações da física mecânica','Aqui estão as principais equações da física mecânica','Aqui estão as principais equações da física mecânica','sendFisicEquationsToInterface');
+INSERT INTO `interactions` VALUES (1,'ola','ola','ola','olá chefe','olá chefe','olá chefe',''),(2,'horas','sao','sao',' ',' ',' ','dateTime'),(4,'mostr','interacoes','interacoes','Atualmente essas são as interações contidas no meu banco de dados','Essas são todas as interações contidas no meu banco de dados','Aqui estão as interações contidas no meu banco de dados','sendInteractionsToInterface'),(5,'mostr','tarefa','tarefa','Essas são todas as suas tarefas pendentes','Aqui estão as suas tarefas pendentes','Atualmente essas são suas tarefas pendentes','sendHomeWorksToInterface'),(6,'mostr','projeto','projeto','Esses são todos os projetos em andamento','Aqui estão os projetos em desenvolvimento','Atualmente esse são os projetos em desenvolvimento','sendProjectsToInterface'),(7,'mostr','device','device','Esses são os devices sincronizados','Aqui estão os devices sincronizados','Atualmente esse são os devices sincronizados com o meu sistema','sendDevicesToInterface'),(8,'fech','image','image','','','','sendCloseToInterface'),(9,'mostr','tabela','periodica','tabela periodica aberta','tabela periodica aberta','tabela periodica aberta','sendPeriodicTableToInterface'),(10,'mostr','mapa','brasil politico','Esse é o mapa político do Brasil','Esse é o mapa político do Brasil','Esse é o mapa político do Brasil','sendPoliticalBrazilToInterface'),(11,'mostr','distribuicao','eletronica','Aqui está a distribuição eletrônica dos elementos','Aqui está a distribuição eletrônica dos elementos','Aqui está a distribuição eletrônica dos elementos','sendEletronicDestToInterface'),(12,'mostr','tabela','angulos','Aqui está a tabela dos principais angulos na trigonometria','Aqui está a tabela dos principais angulos na trigonometria','Aqui está a tabela dos principais angulos na trigonometria','sendAnglesTableToInterface'),(13,'mostr','equacoes','fisica mecanica','Aqui estão as principais equações da física mecânica','Aqui estão as principais equações da física mecânica','Aqui estão as principais equações da física mecânica','sendFisicEquationsToInterface'),(14,'atualiz','sistema','sistema','Certo. Estou baixando as novas atualizações do meu sistema','Certo. Estou baixando as novas atualizações do meu sistema','Certo. Estou baixando as novas atualizações do meu sistema','updateFriday');
 /*!40000 ALTER TABLE `interactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,10 +110,10 @@ DROP TABLE IF EXISTS `projects`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `projects` (
   `ProjectID` int NOT NULL AUTO_INCREMENT,
+  `ProjectType` varchar(40) DEFAULT NULL,
   `ProjectName` varchar(60) DEFAULT NULL,
-  `ProjectRepository` varchar(80) DEFAULT NULL,
   PRIMARY KEY (`ProjectID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,7 +122,7 @@ CREATE TABLE `projects` (
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,'Project','language'),(2,'NewProject','Python');
+INSERT INTO `projects` VALUES (2,'tese','teste'),(3,'tes','test');
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -135,4 +135,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-21 20:46:15
+-- Dump completed on 2020-07-23 20:31:04
